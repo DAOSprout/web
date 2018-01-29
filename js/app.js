b88bf25a077843fd3acecbcba1779b09eeb0a739
Brian Taylor <objectorange@protonmail.com>
2018-01-29 00:55:21 -0700

    collapse js into app.js; update app module name

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 'use strict';
 
+//window.addEventListener('load', function() {
+
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    //if (typeof web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        //web3js = new Web3(web3.currentProvider);
+    //} else {
+    //    console.log('No web3? You should consider trying MetaMask!')
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        //web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    //}
+
+    // Now you can start your app & access web3 freely:
+    //startApp()
+
+//});
+
 // angular.js main app initialization
-var app = angular.module('SC',['ngRoute']);
+var app = angular.module('DS',['ngRoute']);
 
 // routes
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     //$locationProvider.hashPrefix('!');
     $routeProvider.
-            when('/',{templateUrl: 'templates/home.html', controller: 'mainController' }).
-            when('/home',{templateUrl: 'templates/home.html', controller: 'mainController' }).
-            when('/registry',{templateUrl: 'templates/registry.html', controller: 'mainController' }).
-            when('/lists',{templateUrl: 'templates/lists.html', controller: 'mainController' }).
-            when('/sprouts',{templateUrl: 'templates/sprouts.html', controller: 'mainController' }).
-            otherwise({redirectTo: '/'})
-    }]);
+    when('/',{templateUrl: 'templates/home.html', controller: 'mainController' }).
+    when('/home',{templateUrl: 'templates/home.html', controller: 'mainController' }).
+    when('/registry',{templateUrl: 'templates/registry.html', controller: 'mainController' }).
+    when('/lists',{templateUrl: 'templates/lists.html', controller: 'mainController' }).
+    when('/sprouts',{templateUrl: 'templates/sprouts.html', controller: 'mainController' }).
+    otherwise({redirectTo: '/'})
+}]);
 
 // controllers
 app.controller('mainController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
@@ -30,3 +47,11 @@ app.controller('mainController', ['$scope', '$http', '$location', function ($sco
         return $location.path() === '/';
     }
 }]);
+
+// services
+
+
+// filters
+
+
+// directives
